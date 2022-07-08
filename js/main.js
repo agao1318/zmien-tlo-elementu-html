0d96470ac14ff617e7e378e11e70c4daabb4fe18
agao1318 <agao1318@gmail.com>
2022-07-08 10:51:17 +0200

    zmiana

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ console.log(`warsztat ustaw tło elementów HTML`);
 const setBackground = () => {
     let p1 = document.querySelector('.first');
     let p2 = document.querySelector('.last');
-    
+
 p1.classList.add('bg-red');
+p2.classList.add('bg-yellow');
     // p1.style.backgroundColor = 'red';
     // p2.style.backgroundColor = 'yellow';
+    // w projekcie jest p2
     
 }  
 
 let btnSetBackground = document.getElementById('set-background');
 
-btnSetBackground.addEventListener('click', setBackground);
+btnSetBackground.addEventListener('click', setBackground);     
